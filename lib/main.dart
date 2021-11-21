553aa41b4b34dfefea7ce5342db62194c6f1c4ec
Eric Ciarla <ericciarla@yahoo.com>
2021-11-21 12:54:58 -0500

    position updates

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
+  DatabaseService().getPermission();
   runApp(MyApp());
 }
 
